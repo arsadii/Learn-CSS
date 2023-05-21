9d326b693b66621261262fe8acb92cdcca5bdfd8
Arsadi <arsadif55117032@gmail.com>
2023-05-22 07:59:42 +0800

    Revert "responsive navbar"
    This reverts commit b74cf7caf053cac682fff718c62c77e2595cd6c1.

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById("menu-icon");
-const menuList = document.getElementById("menu-list");
-
-menuIcon.addEventListener("click", () => {
-  menuList.classList.toggle("hidden");
-});
